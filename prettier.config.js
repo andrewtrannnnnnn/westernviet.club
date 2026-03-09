d52adcc70c3b3a6579b43036ab1da06c192e4de8
Andrew Tran <andrewtrann233@gmail.com>
2026-03-09 10:45:15 -0700

    chore: add prettier for consistent code formatting

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const prettierConfig = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+};
+
+export default prettierConfig;
